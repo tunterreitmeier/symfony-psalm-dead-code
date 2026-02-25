f6a31f858e42a9293178571eeaae8d7a21be496d
Thomas Unterreitmeier <t.unterreitmeier@icloud.com>
2026-02-25 04:50:08 +0100

    add message handler with unused method

---
+++ src/Handler/MyHandler.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use App\Message\MyMessage;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+final class MyHandler
+{
+    /** @psalm-suppress UnusedParam */
+    #[AsMessageHandler]
+    public function onMyMessage(MyMessage $message): void
+    {
+        // noop
+    }
+
+    public function unusedMethod(): void
+    {
+
+    }
+}
