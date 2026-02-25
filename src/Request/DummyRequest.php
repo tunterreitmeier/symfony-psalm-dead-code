cfc09907d58f0396a06b6241f8ef8b5473f61d9d
Thomas Unterreitmeier <t.unterreitmeier@icloud.com>
2026-02-25 06:28:57 +0100

    add example of false positive unused validator callback method

--- src/Request/DummyRequest.php
+++ src/Request/DummyRequest.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Request;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 final class DummyRequest
 {
     public function __construct(public string $foo)
@@ -14,5 +16,12 @@ final class DummyRequest
     public function unusedMethod(): string
     {
         return 'I am correctly flagged, as Psalm sees static usage of this class';
+    }
+
+    /** @psalm-suppress PossiblyUnusedMethod - false positive - called by validator */
+    #[Assert\Callback]
+    public function validate(): bool
+    {
+        return true;
     }
 }
