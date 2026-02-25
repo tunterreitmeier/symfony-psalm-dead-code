3fe35072dedcb0a79fe430e3e15a1b4ee6dc9840
Thomas Unterreitmeier <t.unterreitmeier@icloud.com>
2026-02-25 04:43:10 +0100

    add event listener with unused method

---
+++ src/Listener/MyListener.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Listener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+#[AsEventListener]
+final class MyListener
+{
+    /** @psalm-suppress UnusedParam */
+    public function __invoke(RequestEvent $event): void
+    {
+        //noop
+    }
+
+    public function unusedMethod(): void
+    {
+
+    }
+}
