222f2f836b5cf6fb239bf2b64da0cf7e9a49e173
Thomas Unterreitmeier <t.unterreitmeier@icloud.com>
2026-03-01 07:00:18 +0100

    add service setter method via required attribute

--- src/Controller/DummyController.php
+++ src/Controller/DummyController.php
@@ -5,20 +5,32 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Request\DummyRequest;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Contracts\Service\Attribute\Required;
 
 #[Route('/dummy', name: 'dummy')]
 class DummyController extends AbstractController
 {
+    private LoggerInterface $logger;
+
     public function __invoke(Request $request, DenormalizerInterface $denormalizer): JsonResponse
     {
         $payload = $denormalizer->denormalize($request->toArray(), DummyRequest::class);
 
+        $this->logger->debug('debug');
+
         return $this->json($payload->foo);
+    }
+
+    #[Required]
+    public function setService(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 
     public function unusedMethod(\mysqli $mysqli): void
